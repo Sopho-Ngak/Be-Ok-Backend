cd8fd013516b44ce6d8d2860ddb5044152bd5016
Sophonie Ngakoutou <sopho.ngak@gmail.com>
2024-04-29 10:49:18 +0200

    bug: fix appointment creation issue

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
@@ -17,6 +19,10 @@ from utils.generate_code import get_random_code
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+
+    if 'loaddata' in sys.argv:
+            return
+    
     if created:
         user_type = instance.user_type
         ProfilePicture.objects.get_or_create(user=instance)
